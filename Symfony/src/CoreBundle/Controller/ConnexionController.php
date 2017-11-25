4b57fc5df863cf044aca51c14074cd9dbd04303b
Paul Merlin <paul.merlin1@etu.univ-lorraine.fr>
2017-11-25 10:36:57 +0100

    Ajout controleur Partie

--- Symfony/src/CoreBundle/Controller/ConnexionController.php
+++ Symfony/src/CoreBundle/Controller/ConnexionController.php
@@ -9,10 +9,10 @@ use CoreBundle\Entity\Joueur;
 
 class ConnexionController extends Controller {
 
-    public function indexAction(Request $request) {
+    public function logAction(Request $request) {
 
         $joueur = new Joueur();
-        
+
         $form = $this->createForm(JoueurType::class, $joueur);
 
         if ($request->isMethod('POST')) {
@@ -33,7 +33,33 @@ class ConnexionController extends Controller {
         }
 
 
-        return $this->render('CoreBundle:Connexion:index.html.twig', array(
+        return $this->render('CoreBundle:Connexion:connexion.html.twig', array(
+                    'form' => $form->createView(),
+        ));
+    }
+
+    public function signAction(Request $request) {
+        $joueur = new Joueur();
+
+        $form = $this->createForm(JoueurType::class, $joueur);
+
+        if ($request->isMethod('POST')) {
+
+            $form->handleRequest($request);
+
+            if ($form->isValid()) {
+                $em = $this->getDoctrine()->getManager();
+                $em->persist($joueur);
+                $em->flush();
+
+                $request->getSession()->getFlashBag()->add('notice', 'Inscrit');
+
+                return $this->redirectToRoute('connexion_home');
+            }
+        }
+
+
+        return $this->render('CoreBundle:Connexion:inscription.html.twig', array(
                     'form' => $form->createView(),
         ));
     }
---
+++ Symfony/src/CoreBundle/Controller/PartieController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace CoreBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use CoreBundle\Entity\Partie;
+
+class PartieController extends Controller {
+
+    public function addAction() {
+        
+    }
+
+}
